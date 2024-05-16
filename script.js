ddc706f6273d9cfb7b0c2792131b9228904f43ea
Matthew Simmons <75648307+MatthewSimmonsDev@users.noreply.github.com>
2024-05-16 00:05:41 -0400

    Proper Array Functionality
    Array removes old values before pushing.

--- script.js
+++ script.js
@@ -1,48 +1,86 @@
 const cardContainer = document.querySelector("#container");
-const addBookContainer = document.querySelector("#add-book-container")
+const addBookContainer = document.querySelector("#add-book-container");
 const addBookBtn = document.querySelector("#add-book-btn");
 
-const myLibrary = [];
 
-function Book(title){
+let myLibrary = [];
+
+function Book(title, author){
     this.title = title;
-}
-
-function addBookToLibrary(){
-    
-    myLibrary.push(new Book("test"));
-    myLibrary.push(new Book("test2"));
-    myLibrary.push(new Book("test3"));
+    this.author = author;
 }
 
 function displayBookCards(){
     for (let i = 0; i < myLibrary.length; i++) {
-        const bookCard = document.createElement("div")
-        bookCard.className = "book-card"
+        
+        const bookCard = document.createElement("div");
+        bookCard.className = "book-card";
         cardContainer.appendChild(bookCard);
-        bookCard.textContent = myLibrary[i].title;    
+        
+        title = document.createElement("h3");
+        title.classname = "book-card-title";
+        bookCard.appendChild(title)
+        title.textContent = myLibrary[i].title;
+        
+        author = document.createElement("h3");
+        author.classname = "book-card-author";
+        bookCard.appendChild(author);
+        author.textContent = myLibrary[i].author;
     }
+    myLibrary = [];
 }
 
 function hideAddButton(){
-    addBookBtn.setAttribute("style", "display:none")
+    addBookBtn.setAttribute("style", "display:none");
 }
 
-function addBookCard(){
-    const infoCard = document.createElement("div")
-    infoCard.className = "add-book-btn-card" 
-    addBookContainer.appendChild(infoCard)
+function createForm(){
+    const form = document.createElement("form");
+    form.className = 'add-book-btn-card';
+    addBookContainer.appendChild(form);
+    
+    const title = document.createElement("input");
+    title.className = 'book-title';
+    title.setAttribute('type', "text");
+    title.setAttribute('title', "title");
+    title.setAttribute('placeholder', "Book Title");
+    form.appendChild(title);
 
-    infoCard.textContent = "TEST"
-    hideAddButton()
+    const author = document.createElement("input");
+    author.className = 'book-author';
+    author.setAttribute('type', "text");
+    author.setAttribute('author', "author");
+    author.setAttribute('placeholder', "Book Author");
+    form.appendChild(author);
+
+    const submitBtn = document.createElement("input");
+    submitBtn.setAttribute('type', "submit");
+    submitBtn.setAttribute('value', "Submit");
+    form.appendChild(submitBtn);
+    submitBtn.addEventListener("click", function(e) {
+        e.preventDefault();
+        addBookToLibrary();
+        displayBookCards();
+        form.remove();
+        
+        addBookBtn.setAttribute("style", "display:block")
+        // form.style.display = "none"
+    })
+
 }
 
+function addBookToLibrary(){
+    const bookTitle = document.querySelector('.book-title');
+    const bookAuthor = document.querySelector('.book-author');
 
-addBookToLibrary();
-displayBookCards();
+    myLibrary.push(new Book(bookTitle.value,bookAuthor.value))
+    console.log(myLibrary)
+}
+
 
 
 addBookBtn.addEventListener('click', () => {
-    addBookCard();
+    hideAddButton();
+    createForm();
 })
 
