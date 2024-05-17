ae92232dd7370b5dafc37d7aad5ff402454932c9
Matthew Simmons <75648307+MatthewSimmonsDev@users.noreply.github.com>
2024-05-17 07:04:15 -0400

    Delete Button
    Added delete button.

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
-const cardContainer = document.querySelector("#container");
-const addBookContainer = document.querySelector("#add-book-container");
+// This targets the button that says "Add Book"
 const addBookBtn = document.querySelector("#add-book-btn");
+// This is the container that holds the submitted books
+const cardContainer = document.querySelector("#container");
 
-
+// Array for submitted books.
 let myLibrary = [];
 
+// Book constructor
 function Book(title, author){
     this.title = title;
     this.author = author;
 }
 
+// Function to display the books that were entered into the form
 function displayBookCards(){
     for (let i = 0; i < myLibrary.length; i++) {
         
@@ -26,26 +29,41 @@ function displayBookCards(){
         author.classname = "book-card-author";
         bookCard.appendChild(author);
         author.textContent = myLibrary[i].author;
+        
+        const deleteBtn = document.createElement("button");
+        deleteBtn.classname = "delete-book-btn";
+        bookCard.appendChild(deleteBtn);
+        deleteBtn.textContent = "X"
+        
+        deleteBtn.addEventListener('click', () => {
+            bookCard.remove();
+        })
+        
     }
     myLibrary = [];
 }
 
+// When adding a book, this hides the add button to prevent it from being clicked again
 function hideAddButton(){
     addBookBtn.setAttribute("style", "display:none");
 }
 
+// Creates the form where user enters book information
 function createForm(){
+    const addBookContainer = document.querySelector("#add-book-container");
     const form = document.createElement("form");
     form.className = 'add-book-btn-card';
     addBookContainer.appendChild(form);
     
+    // Form section for title of the book
     const title = document.createElement("input");
     title.className = 'book-title';
     title.setAttribute('type', "text");
     title.setAttribute('title', "title");
     title.setAttribute('placeholder', "Book Title");
     form.appendChild(title);
-
+    
+    //Section for author of the book
     const author = document.createElement("input");
     author.className = 'book-author';
     author.setAttribute('type', "text");
@@ -53,10 +71,17 @@ function createForm(){
     author.setAttribute('placeholder', "Book Author");
     form.appendChild(author);
 
+    // Form's submit button
     const submitBtn = document.createElement("input");
     submitBtn.setAttribute('type', "submit");
     submitBtn.setAttribute('value', "Submit");
     form.appendChild(submitBtn);
+
+    /* Form's submit button functionality:
+        1. Adds book to library 
+        2. Displays the books for your viewing pleasure
+        3. Removes the form from view for better design
+    */
     submitBtn.addEventListener("click", function(e) {
         e.preventDefault();
         addBookToLibrary();
@@ -64,21 +89,23 @@ function createForm(){
         form.remove();
         
         addBookBtn.setAttribute("style", "display:block")
-        // form.style.display = "none"
     })
 
 }
 
+// Pushes user entered information to the "myLibrary" array
 function addBookToLibrary(){
     const bookTitle = document.querySelector('.book-title');
     const bookAuthor = document.querySelector('.book-author');
 
     myLibrary.push(new Book(bookTitle.value,bookAuthor.value))
-    console.log(myLibrary)
 }
 
 
-
+/* Event listener for the "Add Book" button.
+    1. Hides itself
+    2. Displays form
+*/
 addBookBtn.addEventListener('click', () => {
     hideAddButton();
     createForm();
